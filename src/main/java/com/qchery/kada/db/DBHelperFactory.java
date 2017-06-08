4689aee346a8b1593670ffb15f82ff2617211ed9
qinrui <chinrui1016@163.com>
2017-06-08 22:19:02 +0800

    变量名更正

--- src/main/java/com/qchery/kada/db/DBHelperFactory.java
+++ src/main/java/com/qchery/kada/db/DBHelperFactory.java
@@ -5,22 +5,22 @@ import java.util.Map;
 
 public class DBHelperFactory {
     
-    private Map<ConnectParam, DBHelper> heplerCache = new HashMap<>();
+    private Map<ConnectParam, DBHelper> helperCache = new HashMap<>();
     
     public DBHelper getDbHelper(ConnectParam connectParam) {
         
-        if (null == heplerCache.get(connectParam)) {
+        if (null == helperCache.get(connectParam)) {
             synchronized (this) {
-                if (null == heplerCache.get(connectParam)) {
+                if (null == helperCache.get(connectParam)) {
                     if (ConnectParam.MYSQL.equals(connectParam.getType())) {
-                        heplerCache.put(connectParam, new MySQLHelper(connectParam));
+                        helperCache.put(connectParam, new MySQLHelper(connectParam));
                     } else if (ConnectParam.ORACLE.equals(connectParam.getType())) {
-                        heplerCache.put(connectParam, new OracleHelper(connectParam));
+                        helperCache.put(connectParam, new OracleHelper(connectParam));
                     }
                 }
             }
         }
         
-        return heplerCache.get(connectParam);
+        return helperCache.get(connectParam);
     }
 }
