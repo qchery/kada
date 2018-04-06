d522a39bfdfbbc7f4433153cbc125543c8c8f4d6
Chery <chinrui1016@163.com>
2018-04-06 11:14:27 +0800

    常用 TypeDescriptor 迁移

--- src/main/java/com/qchery/kada/db/TypeMap.java
+++ src/main/java/com/qchery/kada/db/TypeMap.java
@@ -15,56 +15,30 @@ import java.util.Map;
 public class TypeMap {
     private static Map<Integer, TypeDescriptor> typeCache = new HashMap<>();
 
-    public static final TypeDescriptor STRING = new TypeDescriptor("java.lang", "String", true);
-
-    public static final TypeDescriptor LONG = new TypeDescriptor("java.lang", "Long", true);
-
-    public static final TypeDescriptor BYTE_ARRAY = new TypeDescriptor("java.lang", "Byte[]");
-
-    public static final TypeDescriptor BOOLEAN = new TypeDescriptor("java.lang", "Boolean", true);
-
-    public static final TypeDescriptor DATE = new TypeDescriptor("java.util", "Date");
-
-    public static final TypeDescriptor BIG_DECIMAL = new TypeDescriptor("java.math", "BigDecimal");
-
-    public static final TypeDescriptor DOUBLE = new TypeDescriptor("java.lang", "Double", true);
-
-    public static final TypeDescriptor FLOAT = new TypeDescriptor("java.lang", "Float", true);
-
-    public static final TypeDescriptor INTEGER = new TypeDescriptor("java.lang", "Integer", true);
-
-    public static final TypeDescriptor OBJECT = new TypeDescriptor("java.lang", "Object");
-
-    public static final TypeDescriptor SHORT = new TypeDescriptor("java.lang", "Short", true);
-
-    public static final TypeDescriptor TIME = new TypeDescriptor("java.sql", "Time");
-
-    public static final TypeDescriptor BYTE = new TypeDescriptor("java.lang", "Byte", true);
-
     static {
-        typeCache.put(Types.BIGINT, LONG);
-        typeCache.put(Types.BINARY, BYTE_ARRAY);
-        typeCache.put(Types.BIT, BOOLEAN);
-        typeCache.put(Types.BLOB, BYTE_ARRAY);
-        typeCache.put(Types.CHAR, STRING);
-        typeCache.put(Types.CLOB, STRING);
-        typeCache.put(Types.DATE, DATE);
-        typeCache.put(Types.DECIMAL, BIG_DECIMAL);
-        typeCache.put(Types.DOUBLE, DOUBLE);
-        typeCache.put(Types.FLOAT, FLOAT);
-        typeCache.put(Types.INTEGER, INTEGER);
-        typeCache.put(Types.JAVA_OBJECT, OBJECT);
-        typeCache.put(Types.LONGVARBINARY, BYTE_ARRAY);
-        typeCache.put(Types.LONGVARCHAR, STRING);
-        typeCache.put(Types.NUMERIC, BIG_DECIMAL);
-        typeCache.put(Types.OTHER, OBJECT);
-        typeCache.put(Types.REAL, FLOAT);
-        typeCache.put(Types.SMALLINT, SHORT);
-        typeCache.put(Types.TIME, TIME);
-        typeCache.put(Types.TIMESTAMP, DATE);
-        typeCache.put(Types.TINYINT, BYTE);
-        typeCache.put(Types.VARBINARY, BYTE_ARRAY);
-        typeCache.put(Types.VARCHAR, STRING);
+        typeCache.put(Types.BIGINT, TypeDescriptor.LONG);
+        typeCache.put(Types.BINARY, TypeDescriptor.BYTE_ARRAY);
+        typeCache.put(Types.BIT, TypeDescriptor.BOOLEAN);
+        typeCache.put(Types.BLOB, TypeDescriptor.BYTE_ARRAY);
+        typeCache.put(Types.CHAR, TypeDescriptor.STRING);
+        typeCache.put(Types.CLOB, TypeDescriptor.STRING);
+        typeCache.put(Types.DATE, TypeDescriptor.DATE);
+        typeCache.put(Types.DECIMAL, TypeDescriptor.BIG_DECIMAL);
+        typeCache.put(Types.DOUBLE, TypeDescriptor.DOUBLE);
+        typeCache.put(Types.FLOAT, TypeDescriptor.FLOAT);
+        typeCache.put(Types.INTEGER, TypeDescriptor.INTEGER);
+        typeCache.put(Types.JAVA_OBJECT, TypeDescriptor.OBJECT);
+        typeCache.put(Types.LONGVARBINARY, TypeDescriptor.BYTE_ARRAY);
+        typeCache.put(Types.LONGVARCHAR, TypeDescriptor.STRING);
+        typeCache.put(Types.NUMERIC, TypeDescriptor.BIG_DECIMAL);
+        typeCache.put(Types.OTHER, TypeDescriptor.OBJECT);
+        typeCache.put(Types.REAL, TypeDescriptor.FLOAT);
+        typeCache.put(Types.SMALLINT, TypeDescriptor.SHORT);
+        typeCache.put(Types.TIME, TypeDescriptor.TIME);
+        typeCache.put(Types.TIMESTAMP, TypeDescriptor.DATE);
+        typeCache.put(Types.TINYINT, TypeDescriptor.BYTE);
+        typeCache.put(Types.VARBINARY, TypeDescriptor.BYTE_ARRAY);
+        typeCache.put(Types.VARCHAR, TypeDescriptor.STRING);
     }
 
     public static TypeDescriptor getJavaType(int sqlType) {
--- src/main/java/com/qchery/kada/descriptor/java/TypeDescriptor.java
+++ src/main/java/com/qchery/kada/descriptor/java/TypeDescriptor.java
@@ -6,6 +6,23 @@ package com.qchery.kada.descriptor.java;
  */
 public class TypeDescriptor {
 
+    public static final String PKG_JAVA_LANG = "java.lang";
+    public static final String PKG_JAVA_UTIL = "java.util";
+
+    public static final TypeDescriptor STRING = new TypeDescriptor(PKG_JAVA_LANG, "String", true);
+    public static final TypeDescriptor LONG = new TypeDescriptor("java.lang", "Long", true);
+    public static final TypeDescriptor BYTE_ARRAY = new TypeDescriptor("java.lang", "Byte[]");
+    public static final TypeDescriptor BOOLEAN = new TypeDescriptor("java.lang", "Boolean", true);
+    public static final TypeDescriptor DATE = new TypeDescriptor(PKG_JAVA_UTIL, "Date");
+    public static final TypeDescriptor BIG_DECIMAL = new TypeDescriptor("java.math", "BigDecimal");
+    public static final TypeDescriptor DOUBLE = new TypeDescriptor("java.lang", "Double", true);
+    public static final TypeDescriptor FLOAT = new TypeDescriptor("java.lang", "Float", true);
+    public static final TypeDescriptor INTEGER = new TypeDescriptor("java.lang", "Integer", true);
+    public static final TypeDescriptor OBJECT = new TypeDescriptor("java.lang", "Object");
+    public static final TypeDescriptor SHORT = new TypeDescriptor("java.lang", "Short", true);
+    public static final TypeDescriptor TIME = new TypeDescriptor("java.sql", "Time");
+    public static final TypeDescriptor BYTE = new TypeDescriptor("java.lang", "Byte", true);
+
     private String packageName;
 
     private String typeName;
@@ -26,24 +43,12 @@ public class TypeDescriptor {
         return packageName;
     }
 
-    public void setPackageName(String packageName) {
-        this.packageName = packageName;
-    }
-
     public String getTypeName() {
         return typeName;
     }
 
-    public void setTypeName(String typeName) {
-        this.typeName = typeName;
-    }
-
     public boolean isPrimitive() {
         return primitive;
-    }
-
-    public void setPrimitive(boolean primitive) {
-        this.primitive = primitive;
     }
 
     @Override
